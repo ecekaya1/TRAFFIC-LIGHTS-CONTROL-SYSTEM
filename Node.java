6852e96ff1e22d30465436d8f23d146f60249489
Saniye Ece Kaya <115822012+ecekaya1@users.noreply.github.com>
2024-06-13 13:54:41 +0300

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,13 @@
+package com.example.proje;
+
+public class Node<T> {
+    T data;
+    Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
